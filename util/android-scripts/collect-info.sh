a643a8ea47ae1d02cfb7a4acb7ca6213a5eba444
Ulrich Hornung <hornunguli@gmx.de>
2024-02-27 17:01:58 +0100

    extend collection of information from AVD with lscpu

--- util/android-scripts/collect-info.sh
+++ util/android-scripts/collect-info.sh
@@ -2,12 +2,21 @@
 
 # spell-checker:ignore nextest watchplus PIPESTATUS
 
-echo "$HOME"
+echo "information about AVD:"
+
+log_and_run() {
+    echo "$*:"
+    "$@"
+}
+
+log_and_run lscpu
+log_and_run cat /proc/cpuinfo
+log_and_run echo "$HOME"
 PATH=$HOME/.cargo/bin:$PATH
 export PATH
-echo "$PATH"
-pwd
-command -v rustc && rustc -Vv
-ls -la ~/.cargo/bin
-cargo --list
-cargo nextest --version
+log_and_run echo "$PATH"
+log_and_run pwd
+log_and_run command -v rustc && rustc -Vv
+log_and_run ls -la ~/.cargo/bin
+log_and_run cargo --list
+log_and_run cargo nextest --version
