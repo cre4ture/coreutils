7a4b3a8453282497e117f9126ff755f709532d14
Ulrich Hornung <hornunguli@gmx.de>
2024-04-06 22:12:17 +0200

    cspell

--- tests/common/unix/mod.rs
+++ tests/common/unix/mod.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-//spell-checker: ignore xpixel ypixel Openpty
+//spell-checker: ignore xpixel ypixel Openpty winsize
 
 use std::{fs::File, io::Write};
 
